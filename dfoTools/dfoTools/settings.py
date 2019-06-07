c8b5bcab82d8588b0448121ce6dc22ff3f07b54e
Martin <mcywong@uwaterloo.ca>
2019-06-07 15:04:47 -0700

    setup connection to mysql and completed migration

--- dfoTools/dfoTools/settings.py
+++ dfoTools/dfoTools/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dfoTools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DFOTOOLS',
+        'USER': 'dfoTools',
+        'PASSWORD': 'df0t00ls?',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
