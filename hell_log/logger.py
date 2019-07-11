e54e0dce0f8c6d64f64ec831714f94bf8b7ea07f
Martin <mcywong@uwaterloo.ca>
2019-07-11 16:30:49 -0700

    update styling and add new fields to model forms

--- hell_log/logger.py
+++ hell_log/logger.py
@@ -7,3 +7,18 @@ class HellForm(ModelForm):
     class Meta:
         model = HellRuns
         exclude = ['character', 'Date']
+        labels = {
+            'HellType': 'Hell Type',
+            'Runs': 'Hell Runs Done',
+            'EpicDrops': 'Epic Gears Dropped',
+            'HellOrb': 'Hell Orbs',
+            'StoneBox': 'Rift Sensor Stone Boxes',
+            'EpicSoul': 'Epic Souls',
+            'SkyFrags': 'Sky Fragment Bombs',
+            'AntimatterParticle': 'Antimatter Particle Drops',
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(HellForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
